23341522c66b31695358a3d1e37728a2ef062940
Pufferfish101007 <50246616+pufferfish101007@users.noreply.github.com>
2020-07-21 19:14:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express"); const app = express();
 
 app.get("/", (request, response) => {
-response.sendFile("__dirname" + "/views/index.php") });
+response.sendFile("__dirname" + "/views/index.html") });
 
 const listener = app.listen (process.env.PORT, () => { console.log("Your app is listening on port " + listener.address ().port) });*/
